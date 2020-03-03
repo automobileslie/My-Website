2a81dfaefe749a078bd51cca23c9b9ba100dcf1f
Carlie Anglemire <carlieanglemire@Carlies-MacBook-Air.local>
2020-03-02 21:58:14 -0500

    another Monday commit

--- src/Home.js
+++ src/Home.js
@@ -21,9 +21,9 @@ render(){
 
                     <div className="profile-description">
                     <p>Carlie Anglemire is a full stack web developer who graduated from the Software Engineering Program at the Flatiron School.
-                    She enjoys creating web appliations that are user-friendly with React and Ruby on Rails. She could, and sometimes does, spend all day splitting strings.</p>
+                    She enjoys creating web applications that are user-friendly with React and Ruby on Rails. She could, and sometimes does, spend all day splitting strings.</p>
                     <p>
-                    In Carlie's former life she earned a PhD in Philosophy at Stony Brook University, writing a dissertation centered on topics in Ethics and Aesthetics. A book and an essay by Jean-Luc Nancy that she translated from French into English while at Stony Brook were published by Fordham University Press. Carlie also taught Philosophy to undergraduate students at a few universities, and her experiences as an educator inform the work she does now.</p>
+                    In Carlie's former life she earned a PhD in Philosophy at Stony Brook University, writing a dissertation centered on topics in Ethics and Aesthetics. A book and an essay by Jean-Luc Nancy that she translated from French into English while at Stony Brook were published by Fordham University Press. Carlie also taught Philosophy at a few universities, and her experiences as an educator inform the work she does now.</p>
                     <p>Other than programming, some of the things that Carlie loves include learning languages, writing, cooking, and gardening.</p>
                     </div>
             
