35e97f638c8e07f32b1babc778065d74edb348ac
automobileslie <carlie.anglemire@gmail.com>
2020-06-17 17:57:44 -0400

    new images for blog

--- src/App.js
+++ src/App.js
@@ -21,7 +21,9 @@ export default class App extends React.Component{
 
   componentDidMount=()=>{
 
-    fetch("https://tranquil-citadel-59605.herokuapp.com/projects")
+    fetch("http://localhost:3000/projects")
+
+    // fetch("https://tranquil-citadel-59605.herokuapp.com/projects")
     .then(r=>r.json())
     .then(data=>{      
       this.setState({
@@ -29,7 +31,8 @@ export default class App extends React.Component{
       })
     })
 
-    fetch("https://tranquil-citadel-59605.herokuapp.com/posts")
+    fetch("http://localhost:3000/posts")
+    // fetch("https://tranquil-citadel-59605.herokuapp.com/posts")
         .then(r=>r.json())
         .then(the_posts=>{
     
