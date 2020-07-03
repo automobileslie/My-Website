6c14a64bd70957131a3c6caeb0b7e7a5cb74b0a8
automobileslie <carlie.anglemire@gmail.com>
2020-07-02 22:55:06 -0400

    changed url for fetches back

--- src/App.js
+++ src/App.js
@@ -21,9 +21,9 @@ export default class App extends React.Component{
 
   componentDidMount=()=>{
 
-    fetch("http://localhost:3000/projects")
+    // fetch("http://localhost:3000/projects")
 
-    // fetch("https://tranquil-citadel-59605.herokuapp.com/projects")
+    fetch("https://tranquil-citadel-59605.herokuapp.com/projects")
     .then(r=>r.json())
     .then(data=>{      
       this.setState({
@@ -31,8 +31,8 @@ export default class App extends React.Component{
       })
     })
 
-    fetch("http://localhost:3000/posts")
-    // fetch("https://tranquil-citadel-59605.herokuapp.com/posts")
+    // fetch("http://localhost:3000/posts")
+    fetch("https://tranquil-citadel-59605.herokuapp.com/posts")
         .then(r=>r.json())
         .then(the_posts=>{
     
