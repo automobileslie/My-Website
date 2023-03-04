83c6e1d9edb95e4c61a8ace6c28bff34fd05893a
automobileslie <carlie.anglemire@gmail.com>
2023-03-04 08:37:26 -0600

    updating website

--- src/About.js
+++ src/About.js
@@ -44,18 +44,19 @@ render(){
             <div className="second-container-for-about-page">
 
                     <div className="profile-description">
-                    <p>Carlie Anglemire is a full-stack software developer passionate about exercising problem-solving skills and creativity in building applications that help users gain access to information, entertainment, resources, and social networks on-line​. 
-                    With experience in HTML, CSS, JavaScript, Ruby, Python, Java, C, React, Ruby on Rails, and SQL, she became interested in software engineering after developing her logic and critical thinking skills as a Philosophy PhD focused on topics in ethics and aesthetics.</p> 
-                    <p>In other words, she could, and sometimes does, spend all day splitting strings.</p>
-                    <p>In addition to being a software engineer, Carlie is a published translator and loves writing, learning languages, cooking, and gardening.</p>
+                    <p>I am a full-stack software developer who enjoys exercising problem-solving skills and creativity while working for a software company centered on women's health and prenatal care​. 
+                    With experience in a variety of languages and frameworks, I became interested in software engineering after developing logic and critical thinking skills as a Philosophy PhD focused on topics in ethics and aesthetics.</p> 
+                    <p>In other words, I could, and sometimes do, spend all day splitting strings.</p>
+                    <p>In addition to being a software engineer, I am a published translator and love writing, singing and playing the guitar, biking, learning languages, cooking, and gardening.</p>
                     </div>
             
                     <div className="links-on-about-page">
-                    <p className="link-on-about-page" onClick={this.props.displayResume}>Resume</p>
-                    <p>|</p>
                     <p className="link-on-about-page"><a className="link" href="https://github.com/automobileslie" target = "_blank" rel="noopener noreferrer"> Github</a></p> 
-                    <p>|</p>
-                    <p className="link-on-about-page"><a className="link" href="http://www.linkedin.com/in/carlie-anglemire" target = "_blank" rel="noopener noreferrer"> LinkedIn</a></p>                     
+                    <p>| </p>
+                    <p className="link-on-about-page"><a className="link" href="http://www.linkedin.com/in/carlie-anglemire" target = "_blank" rel="noopener noreferrer"> LinkedIn</a></p>  
+                    <p>| </p>
+                    <p className="link-on-about-page"><a className="link" href="https://medium.com/@carlie.anglemire" target = "_blank" rel="noopener noreferrer">Blog</a></p>
+                   
                     </div>
 
 
